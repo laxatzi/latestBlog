87fd032d055711dd82794039caf8fd1e68b3fbfe
laxatzi <laxatzi@yahoo.com>
2023-09-08 18:52:57 +0300

    MINOR-changes to search.js file

--- JS/search.js
+++ JS/search.js
@@ -1,5 +1,7 @@
 const searchTrigger = document.querySelector(".search-trigger");
 const searchOverlay = document.querySelector(".search-overlay");
 
-const callback = function () {};
+const callback = function () {
+  alert("Hi all!");
+};
 searchTrigger.addEventListener("click", callback);
