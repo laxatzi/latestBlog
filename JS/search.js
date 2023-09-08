e27c0c9d805546a0716775da615b7f710c6ea183
laxatzi <laxatzi@yahoo.com>
2023-09-08 16:43:51 +0300

    ADD-variable to search.js for manipulate the searchTrigger

---
+++ JS/search.js
@@ -0,0 +1,5 @@
+const searchTrigger = document.querySelector(".search-trigger");
+const searchOverlay = document.querySelector(".search-overlay");
+
+const callback = function () {};
+searchTrigger.addEventListener("click", callback);
