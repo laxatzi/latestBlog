04c4f356fd2b2d1659873281853e3a3f2aea9c8e
laxatzi <laxatzi@yahoo.com>
2023-09-11 16:42:29 +0300

    REFACTOR- search functions

--- JS/search.js
+++ JS/search.js
@@ -1,7 +1,25 @@
 const searchTrigger = document.querySelector(".search-trigger");
+const closer = document.querySelector(".closer");
 const searchOverlay = document.querySelector(".search-overlay");
+const inputField = document.querySelector("#search");
+const searchGlass = document.querySelector(".input-field span");
+const body = document.querySelector("body");
 
-const callback = function () {
-  alert("Hi all!");
+const openSearch = function () {
+  if (searchOverlay.classList.contains("search-overlay--mute")) {
+    searchOverlay.classList.add("search-overlay--active");
+    searchOverlay.classList.remove("search-overlay--mute");
+    body.setAttribute("id", "no-scroll");
+  } else return;
 };
-searchTrigger.addEventListener("click", callback);
+
+const closeSearch = function () {
+  if (searchOverlay.classList.contains("search-overlay--active")) {
+    searchOverlay.classList.add("search-overlay--mute");
+    searchOverlay.classList.remove("search-overlay--active");
+  } else return;
+};
+
+searchTrigger.addEventListener("click", openSearch);
+closer.addEventListener("click", closeSearch);
+searchGlass.addEventListener("click", () => console.log(inputField.value));
